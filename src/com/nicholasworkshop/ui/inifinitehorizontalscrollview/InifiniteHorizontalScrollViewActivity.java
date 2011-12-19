1b5b202ec844e31f63e328dfcdf27d699e453162
Nicholas Wong <nickwph@gmail.com>
2011-12-19 16:38:50 +0800

    First commit.
    Snapping works.

---
+++ src/com/nicholasworkshop/ui/inifinitehorizontalscrollview/InifiniteHorizontalScrollViewActivity.java
@@ -0,0 +1,13 @@
+package com.nicholasworkshop.ui.inifinitehorizontalscrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InifiniteHorizontalScrollViewActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
